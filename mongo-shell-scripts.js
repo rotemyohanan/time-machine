012f2cf388de1591a3dbd25974be071ebce43f6a
Rotemy <RotemYo@icloud.com>
2017-04-07 05:04:37 +0300

    Delete mongo-shell-scripts.js

--- mongo-shell-scripts.js
+++
@@ -1,9 +0,0 @@
-//using java-script in the mongo shell
-
-var glbal = db.getCollection('binaryObject').find({});
-glbal.forEach( function(it){ print(it.name) });
-
-var glbal = db.getCollection('binaryObject').find({}).forEach(function(binary){
-    var bin =  binary.name;
-    print("name " + bin);
-});
