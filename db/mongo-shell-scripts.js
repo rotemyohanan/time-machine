2527bc06f0d57930e197cf6fb78d3cd1669b950f
Rotemy <RotemYo@icloud.com>
2017-04-07 06:35:23 +0300

    iterate over a collection to get the key values

--- db/mongo-shell-scripts.js
+++ db/mongo-shell-scripts.js
@@ -1,8 +1,25 @@
 //using java-script in the mongo shell
-
-// Simple examples
-
+// ---------------------------------------------------------------------------------------------------
+//  Simple examples
+// ---------------------------------------------------------------------------------------------------
 var glbal = db.getCollection('binaryObject').find({}).forEach(function(binary){
     var bin =  binary.name;
     print("name " + bin);
 });
+
+// ---------------------------------------------------------------------------------------------------
+//  Iterate all the key value in documents, and run a query on each value.
+// ---------------------------------------------------------------------------------------------------
+var keys = [];
+
+db.doors.find({}).forEach(function(doc){
+    for (var key in doc){ 
+      if(typeof(doc[key]) == 'number' && doc[key] > 20000){
+       keys.push({"doc.id": doc._id, "key":key, "value":doc[key]});
+      }
+    }
+});
+print(keys);   // prints all the results
+
+// ---------------------------------------------------------------------------------------------------
+// ---------------------------------------------------------------------------------------------------
