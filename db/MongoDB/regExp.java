158cfaf9ef6b0f412b5b0452faa5b10e42ec6bba
Rotemy <RotemYo@icloud.com>
2017-06-27 12:07:43 +0300

    Update regExp.java

--- db/MongoDB/regExp.java
+++ db/MongoDB/regExp.java
@@ -1,7 +1,11 @@
-
-
+// Links  
+// http://hmkcode.com/mongodb-java-querying/ -- querying OR, AND, ... 
 
 String personIdentifier = "aba";
 def people = Person.getCollection()
 BasicDBObject query = new  Document("username",  java.util.regex.Pattern.compile(personIdentifier))
 def results = people.find(query).sort() // returns order list of the persons in the people collection meets the regexp pattern. 
+
+  
+  
+  
