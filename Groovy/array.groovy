9713ddd940e208aa62640a1820058c2e52343fa0
Rotemy <RotemYo@icloud.com>
2017-07-14 23:43:44 +0300

    Create array.groovy

---
+++ Groovy/array.groovy
@@ -0,0 +1,12 @@
+
+
+// -------------  Array  ---------------- //
+Groovy reuses the list notation for arrays, but to make such literals arrays, you need to explicitely define the type of the array through coercion or type declaration.
+
+String[] arrStr = ['Ananas', 'Banana', 'Kiwi']
+assert arrStr instanceof String[]    
+assert !(arrStr instanceof List)
+// as operator defines the type of the array
+def numArr = [1, 2, 3] as int[]      
+assert numArr instanceof int[]
+
