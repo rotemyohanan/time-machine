3255cf8ebe0bd6f30b0cdde0d73e5212e6cd7164
Rotemy <RotemYo@icloud.com>
2017-07-15 18:46:01 +0300

    Create cheatsheet.js

---
+++ javascript/cheatsheet.js
@@ -0,0 +1,68 @@
+
+
+// javascript
+
+// comments
+// Single line comment
+/*
+ Multi line  comment
+*/
+
+ 
+// ============================================================================================== 
+//  var keyword
+// ============================================================================================== 
+
+variable type is inferred
+
+without the var it is a global variable
+Global variable - can be accessed anywhere in the program. cause coupling between variables have nothing to share in common, defecault to debug.
+
+using the var is a best practice. (solve collisions in a large program)
+
+
+//================================================================================================
+//    DataTypes
+//================================================================================================
+- null
+- undefined
+- String
+
+
+typeof(variable) // returns the datatype of the variable.
+
+Strings:
+
+concatenation using '+'
+no multi line String
+using '\n' to skip line
+
+// ============================================================================================== 
+//  function
+// ============================================================================================== 
+
+fundtion add(first, second) {
+    return first + second;
+}
+
+Note:
+----
+decalration of a variable inside a function without the var , automatically decleare the variable as Global Variable.
+
+
+// ============================================================================================== 
+//  Array
+// ============================================================================================== 
+Array.prototype
+
+
+// ============================================================================================== 
+//  Window
+// ============================================================================================== 
+When js executes in a browser the 'window' object is the global context.
+
+
+// ============================================================================================== 
+//  Array
+// ============================================================================================== 
+Array.prototype
