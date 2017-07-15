ddd7f151faf3174745f22831632a6df8043fe06b
Rotemy <RotemYo@icloud.com>
2017-07-15 18:25:50 +0300

    Update spock.groovy

--- spock/spock.groovy
+++ spock/spock.groovy
@@ -57,3 +57,48 @@ class DataDriven extends Specification {
         0 | 0 || 0
     }
 }
+
+    
+    
+    Spock
+
+
+class Name extends Specification
+
+The DirtiesRuntime annotation
+Test methods may be marked with the grails.test.runtime.DirtiesRuntime annotation to indicate that the test modifies the runtime in ways which might be problematic for other tests and as such the runtime should be refreshed after this test method runs
+
+Annotations
+
+@TestFor()
+The TestFor annotation will also automatically annotate any public methods starting with "test" with JUnit 4's @Test annotation
+    
+    
+    
+    
+ // -------------------- SPOCK - UNIT TESTS -------------------- //
+
+inherit from the Specifications class
+
+@Shared filedName = new Object() // Share the object with all the feature methods.
+equals to initializing the instance in the setupSpec() method.
+
+Static fields should be only used for constants.
+
+def setup() {}          // run before every feature method
+def cleanup() {}        // run after every feature method
+def setupSpec() {}     // run before the first feature method
+def cleanupSpec() {}   // run after the last feature method\\
+
+Feature methods are the heart of a specification
+
+Blocks: Conceptual phases ('when:', 'then:', 'where:', 'setup:', 'given:')
+
+setup: (alias of 'given')
+The setup block is where you do any setup work.
+
+Conditions : just like assertions in JUnit
+
+
+then:
+notThrown(NullPointerException)
