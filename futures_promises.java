a2235f7e35133b4083b89bf95f5f34a8e904afc0
Rotemy <RotemYo@icloud.com>
2017-07-15 21:32:04 +0300

    Create futures_promises.java

---
+++ futures_promises.java
@@ -0,0 +1,14 @@
+
+
+// ---------- Java  general data ---------- //
+/*
+The static import declaration is analogous to the normal import declaration. Where the normal import declaration imports classes from packages, allowing them to be used without package qualification, the static import declaration imports static members from classes, allowing them to be used without class qualification.
+*/
+
+import static grails.async.Promises.task
+
+/*
+ Futures are very important abstraction, even more these day than ever due to growing demand for asynchronous,
+ event-driven, parallel and scalable systems.
+
+*/
