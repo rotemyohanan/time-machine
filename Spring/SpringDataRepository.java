9ab5aad0dd62fa1503cd31e5e8ca5abb4c1339ba
Rotemy <RotemYo@icloud.com>
2017-07-20 22:05:33 +0300

    Create SpringDataRepository.java

---
+++ Spring/SpringDataRepository.java
@@ -0,0 +1,30 @@
+
+
+import org.springframework.data.repository.PagingAndSortingRepository
+
+public interface XXXRepository extends PagingAndSortingRepository<DocumentObject,String> {
+
+   DocumentObject findBySignatureAndProductName(String signature, String ProductName);
+   DocumentObject findByProductiD(String productId);
+   
+}
+
+// The DocumentObject is the model object represent as the Document in the MongoDB
+
+
+
+The DocumentObject should be set as follows:
+@TypeAlias("product")
+@CompoundIndex(name = 'signature_1_productName_1', def="{'signature':1, 'productName':1}", unique = true, background = true)
+@Document(collection = 'product')
+public class Product {
+
+    @Id
+    String id;
+    
+    @Indexed(background = true)
+    String productId;
+    
+    String subjectId;
+    String productName;
+}
